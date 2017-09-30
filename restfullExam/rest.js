bd8064c5ef0f925d4b5448675f6bf3a92c3b2329
yu yunho <freest71@hanmail.net>
2017-09-30 16:15:07 +0900

    db추가예제

--- restfullExam/rest.js
+++ restfullExam/rest.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname+'/public'));
 
 var mysql = require('mysql');
-var mongodb = require('mongodb');
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -13,6 +13,106 @@ var connection = mysql.createConnection({
   user     : 'root',
   password : 'test1234',
   database : 'restful'
+});
+connection.connect();
+
+/////////////////////////////////////////////////
+var MongoClient = require('mongodb').MongoClient;
+// Connection URL 
+var url = 'mongodb://localhost:27017/restful';
+var dbObj = null;
+
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  console.log("Connected correctly to server");
+  dbObj = db;
+});
+app.get('/user/message',function(req,res) {
+	//get방식은 query에서
+	console.log(req.query.sender_id);
+	var condition = {};
+	if (req.query.sender_id != undefined)
+		condition = {sender_id:req.query.sender_id};
+	var messages = dbObj.collection('messages');
+	//messages.find({})  //전체row 조회
+	messages.find(condition)	//?sender_id=2 매개변수로 지정한건만 조회
+	    .toArray(function(err,results){
+		if (err){
+			res.send(JSON.stringify(err));;
+		} else {
+			res.send(JSON.stringify(results));
+		}
+	});
+
+});
+var ObjectID = require('mongodb').ObjectID;
+app.get('/user/message/:id',function(req,res) {
+	var messages = dbObj.collection('messages');
+	messages.findOne(
+		{_id:ObjectID.createFromHexString(req.params.id)},
+		function(err, result){
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+app.post('/user/message',function(req,res) {
+	//post방식은 body에서, query가능하지만 body에 있으므로 사용
+	console.log(req.body.sender_id);
+	console.log(req.body.reciever_id);
+	console.log(req.body.message);
+
+	connection.query('select id,name from user where id=? or id=?',
+		[req.body.sender_id, req.body.reciever_id],
+		function(err, results, fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {	
+				var sender = {};
+				var reciever = {};
+				for (var i = 0; i < results.length; i++) {
+					if (results[i].id == Number(req.body.sender_id)) {
+						sender = results[i];
+					}
+					if (results[i].id == Number(req.body.receiver_id)) {
+						reciever = results[i];
+					}
+				}
+				var object = {
+					sender_id:req.body.sender_id,
+					reciever_id:req.body.reciever_id,
+					sender:sender, reciever:reciever,
+					message:req.body.message,
+					created_at:new Date()					
+				}
+				console.log("__log__object1");
+				var messages = dbObj.collection('messages');
+				messages.save(object, function(err,result) {
+					if (err) {
+						res.send(JSON.stringify(err));
+						console.log("__log__object_err");
+					} else {
+						res.send(JSON.stringify(result));
+						console.log("__log__object_succ");
+					}
+				});
+			}
+		});
+});
+app.delete('/user/message/:id',function(req,res) {
+	//특정row get과 모두 동일하면서 아래 remove만 바꺼주면 됨
+	var messages = dbObj.collection('messages');
+	messages.remove(
+		{_id:ObjectID.createFromHexString(req.params.id)},
+		function(err, result){
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
 });
 
 //RESTfull  -> postman에서 서버 호출
@@ -35,7 +135,7 @@ app.get('/user/:id',function(req,res){
 				res.send(JSON.stringify(err));
 			} else {
 				if (results.length > 0) {
-					results[0].city = '서울';
+					//results[0].city = '서울';
 					res.send(JSON.stringify(results[0]));
 				} else {
 					res.send(JSON.stringify({}));
